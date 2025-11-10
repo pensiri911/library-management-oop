bf7eddc37bb9a9beeb8314e5abec2667a8501b2d
PENNNN <pensiri4447@gmai.com>
2025-11-11 00:33:38 +0700

    Fix file little bit

--- procedural_version/library_procedural.py
+++ procedural_version/library_procedural.py
@@ -4,7 +4,6 @@ class Book():
         self.id = book_id
         self.title = title
         self.author = author
-        self.total_copies = available_copies
         self.available_copies = available_copies
     
     def borrow_book(self):
@@ -21,11 +20,8 @@ class Member():
         self.email = email
         self.borrowed_books = []
 
-    def return_book(self, book):
-        if book.return_book:
-            self.borrowed_books.pop(book)
-            return True
-        return False
+    def borrow(self, book_id):
+        self.borrowed_books.append(book_id)
     
     
 class Library():
@@ -63,28 +59,39 @@ class Library():
         if len(member.borrowed_books) == 3:
             print("Error: Member has reached borrowing limit!")
             return False
-        book.borrow()
-        member.borrowed_books.append(book)
+        book.borrow_book()
+        member.borrow(book.id)
         transaction = {
             'member_id': member_id,
             'book_id': book_id,
-            'member_name': member['name'],
-            'book_title': book['title']
+            'member_name': member.name,
+            'book_title': book.title
         }
         self.borrowed_books.append(transaction)
-        print(f"{member['name']} borrowed '{book['title']}'")
+        print(f"{member.name} borrowed '{book.title}'")
         return True
 
     def return_book(self, member_id, book_id):
         book = self.find_book(book_id)
         member = self.find_member(member_id)
-        if book == None or member == None:
-            pass
-        if member.return_book(book):
-            pass
-        else:
-            pass
         
+        if book == None or member == None:
+            print("Error: Member or book not found!")
+            return False
+        
+        if book_id not in member.borrowed_books:
+            print("Error: This member hasn't borrowed this book!")
+            return False
+        book.return_book()
+        member.borrowed_books.remove(book_id)
+        
+        for i, transaction in enumerate(self.borrowed_books):
+            if transaction['member_id'] == member_id and transaction['book_id'] == book_id:
+                self.borrowed_books.pop(i)
+                break
+        print(f"{member.name} returned '{book.title}'")
+        return True
+    
     def find_book(self, book_id):
         """Find a book by ID"""
         for book in self.books:
